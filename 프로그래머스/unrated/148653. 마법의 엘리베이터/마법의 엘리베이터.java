24a47dc3615d7a8617bbc6b696b7cb206fed903b
ichubtou <106014960+ichubtou@users.noreply.github.com>
2023-01-12 16:07:37 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int solution(int storey) {
+        int sum = 0;
+
+        String str = Integer.toString(storey);
+
+        while(str.length() != 0) {
+            if(str.charAt(str.length() - 1) == '5') {
+                if(str.length() > 1 && str.charAt(str.length() - 2) > '4') {
+                    sum += 10 - str.charAt(str.length() - 1) + '0';
+                    str = Integer.toString(Integer.parseInt(str) + 10);
+                } else {
+                    sum += 5;
+                }
+            } else if(str.charAt(str.length() - 1) > '5') {
+                sum += 10 - str.charAt(str.length() - 1) + '0';
+                str = Integer.toString(Integer.parseInt(str) + 10);
+            } else if(str.charAt(str.length() - 1) < '5') {
+                sum += str.charAt(str.length() - 1) - '0';
+            }
+
+            str = str.substring(0, str.length() - 1);
+        }
+
+        return sum;
+//         int sum = 0;
+
+//         String str = Integer.toString(storey);
+
+//         while(str.length() != 0) {
+//             if(str.charAt(str.length() - 1) == '5') {
+//                 sum += 5;
+//             } else if(str.charAt(str.length() - 1) > '5') {
+//                 sum += 10 - str.charAt(str.length() - 1) + '0';
+//                 str = Integer.toString(Integer.parseInt(str) + 10);
+//             } else if(str.charAt(str.length() - 1) < '5') {
+//                 sum += str.charAt(str.length() - 1) - '0';
+//             }
+
+//             str = str.substring(0, str.length() - 1);
+//         }
+
+//         return sum;
+    }
+}
