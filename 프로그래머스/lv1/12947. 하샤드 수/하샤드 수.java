51ec86125574f43b62a414c7823135197face165
ichubtou <106014960+ichubtou@users.noreply.github.com>
2023-07-14 00:46:14 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        String s = String.valueOf(x);
+        int sum = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            sum += s.charAt(i) - '0';
+        }
+        
+        if(x % sum == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
